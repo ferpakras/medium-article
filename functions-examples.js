f5a472f9bdb2374ffeae22b35542434c2b6fce34
Fernando Krasnievicz <ferpa.88@gmail.com>
2020-01-31 12:57:26 -0300

    Create functions-examples.js

---
+++ functions-examples.js
@@ -0,0 +1,7 @@
+const Parenteses = (nomeDaProp01, nomeDaProp02) => {}
+
+const ParentesesChaves = ({nomeDaProp01, nomeDaProp02}) => {}
+
+const ParentesesChavesObjeto = ({nomeDaProp01, nomeDaProp02},nomeDaProp03) => {}
+
+const PropsSozinha = nomeDaProp => {}
